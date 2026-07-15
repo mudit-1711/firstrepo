bb6429ae5f439aba6dc26072de49917c2c588d32
mudit_1711 <muditagrawal06@gmail.com>
2026-07-15 14:25:06 +0530

    sort

--- dsalab2/selectionsort.cpp
+++ dsalab2/selectionsort.cpp
@@ -1,13 +1,17 @@
+//selection sort
 #include <iostream>
+#include <vector>
 using namespace std;
-void bubbleSort(vector<int>&v) {
+void selectionsort(vector<int>& v) {
     int n = v.size();
     for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr[j], arr[j + 1]);
+        int min_idx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (v[j] < v[min_idx]) {
+                min_idx = j;
             }
         }
+        swap(v[i], v[min_idx]);
     }
 }
 int main() {
@@ -17,12 +21,12 @@ int main() {
     for(i=0;i<n;i++){
         cin>>v[i];
     }
-    bubbleSort(arr, n);
+    selectionsort(v);
     for(i=0;i<n;i++){
         cout<<v[i]<<" ";
     }
     return 0;
 }
-//best case o(n)
+//best case o(n^2)
 // worst case o(n^2)
 //average case o(n^2)
